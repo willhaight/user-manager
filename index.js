29e43decd8b60dd25572185ffd2d6589ffda8d59
willhaight <82068260+willhaight@users.noreply.github.com>
2022-02-10 10:03:08 -0700

    finiliazed changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
-const Client = require('pg').Client
-const client = new Client({
-    user: "postgres",
-    password: "Test123",
-    port: 5432,
-    database: "users"
-})
+// const Client = require('pg').Client
+// const client = new Client({
+//     user: "postgres",
+//     password: "Test123",
+//     port: 5432,
+//     database: "users"
+// })
